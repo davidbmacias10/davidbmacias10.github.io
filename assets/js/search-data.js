b98ca4e2ed841db22564a218bfedd80453f9db97
davidbmacias10 <davidbmacias10@gmail.com>
2025-04-25 23:45:51 +0000

    Deploying to gh-pages from @ davidbmacias10/davidbmacias10.github.io@28fb4a9b3b4d9372b748515150c6d9791dfc92ac 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -451,9 +451,9 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-driving-telematics-and-signal-processing",
+          title: 'Driving Telematics and Signal Processing',
+          description: "Prediction of crashes based on driving behavior.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-project-1",
