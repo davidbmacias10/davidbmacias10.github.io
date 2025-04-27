0efcbb778f7b046e222b1ac5c97baa51ab69a6a9
davidbmacias10 <davidbmacias10@gmail.com>
2025-04-27 04:53:41 +0000

    Deploying to gh-pages from @ davidbmacias10/davidbmacias10.github.io@97125c0285dcc0846d6f72ee2e895cdeb1ee96b8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-projects",
           title: "projects",
-          description: "",
+          description: "Projects that are completed or ongoing related to work or research.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
