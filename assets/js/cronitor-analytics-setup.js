acd00badcb48f11292ed21ed7ed1314031462978
davidbmacias10 <davidbmacias10@gmail.com>
2025-05-02 17:23:44 +0000

    Deploying to gh-pages from @ davidbmacias10/davidbmacias10.github.io@fc5333532c0c6a1642bc31e8f5671c002e876493 🚀

--- assets/js/cronitor-analytics-setup.js
+++ assets/js/cronitor-analytics-setup.js
@@ -3,4 +3,4 @@ window.cronitor =
   function () {
     (window.cronitor.q = window.cronitor.q || []).push(arguments);
   };
-cronitor("config", { clientKey: "" });
+cronitor("config", { clientKey: "cffc0b2f9ff30af7643bedf587b466a1" });
--- assets/js/open-panel-analytics-setup.js
+++ assets/js/open-panel-analytics-setup.js
@@ -4,7 +4,7 @@ window.op =
     (window.op.q = window.op.q || []).push(args);
   };
 window.op("init", {
-  clientId: "",
+  clientId: "83916efe-14c8-4737-a772-1234d1ca192b",
   trackScreenViews: true,
   trackOutgoingLinks: true,
   trackAttributes: true,
